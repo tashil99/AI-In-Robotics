4feb4c53cecc5e062c6ef270f5f92a55c0d9bdd1
tashil99 <tb847@live.mdx.ac.uk>
2025-10-13 09:55:43 +0400

    Test training done on yolov8s model.

--- scripts/training-model.py
+++ scripts/training-model.py
@@ -9,20 +9,20 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 # Load YOLOv8 model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 
 # Train
 results = model.train(
     data="../dataset/data.yaml",
-    epochs=50,
-    imgsz=640,
-    batch=16,
-    name="AI-In-Robotics-Exp1",
+    epochs=5,
+    imgsz=416,
+    batch=8,
+    name="AI-In-Robotics-Exp09",
     verbose=True
 )
 
 # Save results object (pickle)
-results_dir = "runs/detect/AI-In-Robotics-Exp1"
+results_dir = "runs/detect/AI-In-Robotics-Exp09"
 os.makedirs(results_dir, exist_ok=True)
 results_path = os.path.join(results_dir, "results.pkl")
 
